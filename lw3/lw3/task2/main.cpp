c372275276acc2c462a1a3ed7703bbfc0e4791d6
maksim-chernydev <maksim-chernydev@DESKTOP-U2UTPU1>
2024-04-11 02:52:45 +0300

    добавил третье задание третьей лабы

--- lw3/lw3/task2/main.cpp
+++ lw3/lw3/task2/main.cpp
@@ -1,99 +1,161 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <vector>
+#include <numbers>
 
 struct Point 
 {
-    float x, y;
+    float x;
+    float y;
 };
 
-std::vector<Point> ControlPoints = { {100, 100}, {200, 300}, {400, 200}, {500, 400} };
-int SelectedPointIndex = -1;
-bool IsDragging = false;
-Point MousePos;
-
-Point CalculateBezierPoint(float t)
+struct Color
 {
-    float oneMinusT = 1.0f - t;
-    Point p;
-    p.x = oneMinusT * oneMinusT * oneMinusT * ControlPoints[0].x +
-        3 * oneMinusT * oneMinusT * t * ControlPoints[1].x +
-        3 * oneMinusT * t * t * ControlPoints[2].x +
-        t * t * t * ControlPoints[3].x;
+    float red;
+    float green;
+    float blue;
+};
 
-    p.y = oneMinusT * oneMinusT * oneMinusT * ControlPoints[0].y +
-        3 * oneMinusT * oneMinusT * t * ControlPoints[1].y +
-        3 * oneMinusT * t * t * ControlPoints[2].y +
-        t * t * t * ControlPoints[3].y;
-
-    return p;
-}
-
-void MouseButtonCallback(GLFWwindow* window, int button, int action, int mods) 
+class Krosh 
 {
-    if (button == GLFW_MOUSE_BUTTON_LEFT) 
+public:
+    Krosh(Point position, unsigned width, unsigned height, Color bodyColor, Color legColor, Color handColor, Color earColor)
+        : m_position(position)
+        , m_width(width)
+        , m_height(height)
+        , m_bodyColor(bodyColor)
+        , m_legColor(legColor)
+        , m_handColor(handColor)
+        , m_earColor(earColor)
     {
-        if (action == GLFW_PRESS) 
+    };
+
+    void Draw()
+    {
+        glPushMatrix();
+        glTranslatef(m_position.x, m_position.y, 0);
+
+        DrawLegs();
+        DrawEars();
+        DrawBody(); 
+        DrawEyelids();
+        DrawTeeth(); 
+        DrawMouth();
+        DrawEyes();
+        DrawNose();
+        DrawHands();
+
+        glPopMatrix();
+    }
+
+private:
+    Point m_position;
+    unsigned m_width;
+    unsigned m_height;
+    Color m_bodyColor;
+    Color m_legColor;
+    Color m_handColor;
+    Color m_earColor;
+
+    void DrawEyes()
+    {
+        glColor3f(1, 1, 1);
+        FillEllipse(790, 470, 70, 100, 360);
+        glColor3f(0, 0, 0);
+        FillEllipse(820, 470, 14, 20, 360);
+
+        glColor3f(1, 1, 1);
+        FillEllipse(930, 470, 90, 140, 360);
+        glColor3f(0, 0, 0);
+        FillEllipse(880, 470, 18, 28, 360);
+    }
+
+    void DrawEyelids()
+    {
+        glColor3f(0, 0, 0);
+        FillEllipse(790, 350, 70, 100, 360);
+        glColor3f(m_bodyColor.red, m_bodyColor.green, m_bodyColor.blue);
+        FillEllipse(790, 356, 75, 100, 360);
+
+        glColor3f(0, 0, 0);
+        FillEllipse(930, 450, 90, 140, 360);
+        glColor3f(m_bodyColor.red, m_bodyColor.green, m_bodyColor.blue);
+        FillEllipse(930, 456, 95, 140, 360);
+    }
+
+    void DrawMouth()
+    {
+        glColor3f(0, 0, 0);
+        FillEllipse(870, 630, 200, 41, 360);
+        glColor3f(m_bodyColor.red, m_bodyColor.green, m_bodyColor.blue);
+        FillEllipse(870, 627, 200, 40, 360);
+    }
+
+    void DrawTeeth()
+    {
+        glColor3f(1, 1, 1);
+        FillEllipse(900, 680, 40, 50, 360);
+        glColor3f(1, 1, 1);
+        FillEllipse(840, 700, 40, 60, 360);
+    }
+
+    void DrawNose()
+    {
+        glColor3f(1, 0, 0);
+        FillEllipse(850, 550, 20, 20, 360);
+    }
+
+    void DrawBody()
+    {
+        glColor3f(m_bodyColor.red, m_bodyColor.green, m_bodyColor.blue);
+        FillEllipse(850, 600, 370, 370, 360);
+    }
+
+    void DrawLegs()
+    {
+        glColor3f(m_legColor.red, m_legColor.green, m_legColor.blue);
+        FillEllipse(600, 900, 150, 100, 360);
+        FillEllipse(1100, 900, 150, 100, 360);
+    }
+
+    void DrawHands()
+    {
+        glColor3f(m_handColor.red, m_handColor.green, m_handColor.blue);
+
+        glPushMatrix();
+        glRotatef(-10, 0, 0, 1);
+        FillEllipse(1100, 750, 100, 200, 360);
+        glPopMatrix();
+
+        glPushMatrix();
+        glRotatef(10, 0, 0, 1);
+        FillEllipse(600, 450, 100, 200, 360);
+        glPopMatrix();
+    }
+
+    void DrawEars()
+    {
+        glColor3f(m_earColor.red, m_earColor.green, m_earColor.blue);
+        FillEllipse(700, 160, 100, 200, 360);
+        FillEllipse(1000, 160, 100, 200, 360);
+    }
+
+    void FillEllipse(
+        float xCenter, float yCenter, float rx, float ry, int points)
+    {
+        const float step = 2 * std::numbers::pi / points;
+        glBegin(GL_TRIANGLE_FAN);
+        glVertex2d(xCenter, yCenter);
+        for (float angle = 0; angle <= 2 * std::numbers::pi; angle += step)
         {
-            double xpos, ypos;
-            glfwGetCursorPos(window, &xpos, &ypos);
-            MousePos.x = static_cast<float>(xpos);
-            MousePos.y = static_cast<float>(ypos);
-
-            for (size_t i = 0; i < ControlPoints.size(); ++i) 
-            {
-                float dx = MousePos.x - ControlPoints[i].x;
-                float dy = MousePos.y - ControlPoints[i].y;
-                float distanceSquared = dx * dx + dy * dy;
-                if (distanceSquared < 25) 
-                {
-                    SelectedPointIndex = static_cast<int>(i);
-                    IsDragging = true;
-                    break;
-                }
-            }
+            float a = (fabsf(angle - 2 * std::numbers::pi) < 1e-5) ? 0 : angle;
+            const float dx = rx * cosf(a);
+            const float dy = ry * sinf(a);
+            glVertex2f(dx + xCenter, dy + yCenter);
         }
-        else if (action == GLFW_RELEASE) 
-        {
-            IsDragging = false;
-            SelectedPointIndex = -1;
-        }
+        glEnd();
     }
-}
-
-void CursorPositionCallback(GLFWwindow* window, double xpos, double ypos) 
-{
-    if (IsDragging && SelectedPointIndex != -1) 
-    {
-        ControlPoints[SelectedPointIndex].x = static_cast<float>(xpos);
-        ControlPoints[SelectedPointIndex].y = static_cast<float>(ypos);
-    }
-}
-
-void DrawBezierCurve() 
-{
-    glLineWidth(2.0f);
-    glBegin(GL_LINE_STRIP);
-    for (int i = 0; i <= 100; ++i) 
-    {
-        float t = i / 100.0f;
-        Point p = CalculateBezierPoint(t);
-        glVertex2f(p.x, p.y);
-    }
-    glEnd();
-}
-
-void DrawControlPoints() 
-{
-    glPointSize(10);
-    glColor3f(1, 0, 0);
-    glBegin(GL_POINTS);
-    for (const auto& point : ControlPoints) 
-    {
-        glVertex2f(point.x, point.y);
-    }
-    glEnd();
-}
+};
 
 int main() {
     GLFWwindow* window;
@@ -103,7 +165,7 @@ int main() {
         return -1;
     }
 
-    window = glfwCreateWindow(1920, 1080, "Draw Smeshariki", NULL, NULL);
+    window = glfwCreateWindow(1920, 1080, "Draw Krosh", NULL, NULL);
     if (!window) 
     {
         glfwTerminate();
@@ -112,8 +174,7 @@ int main() {
 
     glfwMakeContextCurrent(window);
 
-    glfwSetMouseButtonCallback(window, MouseButtonCallback);
-    glfwSetCursorPosCallback(window, CursorPositionCallback);
+    Krosh krosh({ 100, 40 }, 150, 150, { 0, 0.9, 0.9 }, { 0, 1, 1 }, { 0, 1, 1 }, { 0, 1, 1 });
 
     while (!glfwWindowShouldClose(window)) 
     {
@@ -127,9 +188,7 @@ int main() {
         glLoadIdentity();
         glOrtho(0, width, height, 0, -1, 1);
 
-        glColor3f(1, 1, 1);
-        DrawBezierCurve();
-        DrawControlPoints();
+        krosh.Draw();
 
         glfwSwapBuffers(window);
         glfwPollEvents();
